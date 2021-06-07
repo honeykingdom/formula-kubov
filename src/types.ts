00728b45a3009b1e5f8d1038f7f7e065703a2148
DmitryScaletta <dmitryscaletta@mail.ru>
2021-06-07 23:55:32 +0300

    🏷️ Add types

---
+++ src/types.ts
@@ -0,0 +1,34 @@
+export type User = {
+  email: string;
+  exp: number;
+  iat: number;
+  name: string;
+  picture: string;
+  sub: string;
+};
+
+export type Options = {
+  hostname: string;
+  tvPlayerUrl: string;
+  tvPlayerType: 'embed' | 'sportbox' | 'match.tv';
+  tvPlayerIsPlaylist: boolean;
+  twitchPlayer: string;
+  twitchChats: string[];
+};
+
+export type GetOptionsResponse = {
+  role: Role;
+  options: Options;
+};
+
+export type UpdateOptionsDto = Partial<
+  Pick<Options, 'tvPlayerUrl' | 'twitchPlayer' | 'twitchChats'>
+>;
+
+export type OptionNames = keyof Options;
+
+export type Role = 'admin' | 'moderator';
+
+export type OptionsRow = { name: string; value: string };
+
+export type RolesRow = { userId: string; role: Role };
