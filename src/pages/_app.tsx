30f2c7b0553c73a50729d7553ae21877f79a1db3
DmitryScaletta <dmitryscaletta@mail.ru>
2021-06-08 02:16:45 +0300

    🩹 Fix next-auth error

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,3 +1,6 @@
+// TODO: https://github.com/nextauthjs/next-auth/issues/988#issuecomment-830623141
+import 'core-js';
+import 'regenerator-runtime/runtime';
 import { Provider as AuthProvider } from 'next-auth/client';
 
 const MyApp = ({ Component, pageProps }) => (
