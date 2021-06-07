cca8834e87af7f89ca96bf79d682214f455772c1
DmitryScaletta <dmitryscaletta@mail.ru>
2021-06-08 00:05:24 +0300

    ✨ Add options endpoint

---
+++ src/pages/api/options.ts
@@ -0,0 +1,105 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import jwt from 'next-auth/jwt';
+import fetch from 'node-fetch';
+import type {
+  GetOptionsResponse,
+  OptionsRow,
+  RolesRow,
+  UpdateOptionsDto,
+  User,
+} from 'types';
+import supabase from 'utils/supabase';
+import parseOptions from 'utils/parseOptions';
+import parseRoles from 'utils/parseRoles';
+
+// https://regexr.com/3dj5t
+const YOUTUBE_VIDEO_REGEX = /^((?:https?:)?\/\/)?((?:www|m)\.)?(?:youtube\.com|youtu.be)(\/(?:[\w-]+\?v=|embed\/|v\/)?)([\w-]+)(\S+)?$/;
+
+const secret = process.env.JWT_SECRET;
+
+const getOptionsAndRoles = async () => {
+  const [optionsResponse, rolesResponse] = await Promise.all([
+    supabase.from<OptionsRow>('fk_options').select('*'),
+    supabase.from<RolesRow>('fk_roles').select('*'),
+  ]);
+
+  return [parseOptions(optionsResponse), parseRoles(rolesResponse)] as const;
+};
+
+const isSportBoxPlaylist = async (url: string) => {
+  const response = await fetch(url);
+  const text = await response.text();
+  const isPlaylist = text.includes('//uma.media/playlist/');
+
+  return isPlaylist;
+};
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  const user = (await jwt.getToken({ req, secret })) as User;
+
+  if (!user) {
+    res.status(401);
+
+    return res.end();
+  }
+
+  const [options, roles] = await getOptionsAndRoles();
+
+  if (roles[user.sub] !== 'admin' && roles[user.sub] !== 'moderator') {
+    res.status(401);
+
+    return res.end();
+  }
+
+  if (req.method === 'GET') {
+    const response: GetOptionsResponse = { role: roles[user.sub], options };
+
+    return res.send(response);
+  }
+
+  if (req.method === 'PATCH') {
+    const {
+      tvPlayerUrl,
+      twitchPlayer,
+      twitchChats,
+    }: UpdateOptionsDto = req.body;
+
+    const newOptions: { name: string; value: string }[] = [];
+
+    if (tvPlayerUrl) {
+      if (tvPlayerUrl.startsWith('https://news.sportbox.ru')) {
+        const isPlaylist = await isSportBoxPlaylist(tvPlayerUrl);
+
+        newOptions.push(
+          { name: 'tvPlayerUrl', value: tvPlayerUrl },
+          { name: 'tvPlayerType', value: 'sportbox' },
+          { name: 'tvPlayerIsPlaylist', value: `${isPlaylist}` },
+        );
+      } else {
+        const m = YOUTUBE_VIDEO_REGEX.exec(tvPlayerUrl);
+
+        if (m) {
+          const value = `https://youtube.com/embed/${m[4]}`;
+
+          newOptions.push({ name: 'tvPlayerUrl', value });
+        } else {
+          newOptions.push({ name: 'tvPlayerUrl', value: tvPlayerUrl });
+        }
+
+        newOptions.push({ name: 'tvPlayerType', value: 'embed' });
+      }
+    }
+
+    if (twitchPlayer) {
+      newOptions.push({ name: 'twitchPlayer', value: twitchPlayer });
+    }
+
+    if (twitchChats) {
+      newOptions.push({ name: 'twitchChats', value: twitchChats.join(';') });
+    }
+
+    await supabase.from('fk_options').upsert(newOptions);
+
+    return res.end();
+  }
+};
