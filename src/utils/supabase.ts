134a5a3dcc04b345dd39f30b9fc98c3aee4326ff
DmitryScaletta <dmitryscaletta@mail.ru>
2021-06-08 02:16:21 +0300

    🩹 Use anon key for frontend

--- src/utils/supabase.ts
+++ src/utils/supabase.ts
@@ -1,8 +1,10 @@
 import { createClient } from '@supabase/supabase-js';
 
 const supabase = createClient(
-  process.env.SUPABASE_URL,
-  process.env.SUPABASE_API_KEY,
+  process.env.NEXT_PUBLIC_SUPABASE_URL,
+  typeof window === 'undefined'
+    ? process.env.SUPABASE_API_KEY
+    : process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
 );
 
 export default supabase;
