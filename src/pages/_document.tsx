1ee9d9493c1d76b5ab005d3d787a60321fd4ba0d
DmitryScaletta <dmitryscaletta@mail.ru>
2021-06-08 00:06:55 +0300

    🩹 Remvoe styled-components

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,5 +1,4 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document';
-import { ServerStyleSheet } from 'styled-components';
 
 const gaScript = `
   window.dataLayer = window.dataLayer || [];
@@ -46,31 +45,5 @@ export default class MyDocument extends Document {
         </body>
       </Html>
     );
-  }
-
-  static async getInitialProps(ctx) {
-    const sheet = new ServerStyleSheet();
-    const originalRenderPage = ctx.renderPage;
-
-    try {
-      ctx.renderPage = () =>
-        originalRenderPage({
-          enhanceApp: (App) => (props) =>
-            sheet.collectStyles(<App {...props} />),
-        });
-
-      const initialProps = await Document.getInitialProps(ctx);
-      return {
-        ...initialProps,
-        styles: (
-          <>
-            {initialProps.styles}
-            {sheet.getStyleElement()}
-          </>
-        ),
-      };
-    } finally {
-      sheet.seal();
-    }
   }
 }
